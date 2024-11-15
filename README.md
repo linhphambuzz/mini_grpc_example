716faf142e4eaf5cedb5050eb1c9c20cb8b43ac0
linhphambuzz <mylinh.phamtran@gmail.com>
2024-11-15 14:44:27 -0600

    server registration

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
+# MINI GRPC EXAMPLE 
+
+A small example of writting client/server code implementing the C++ gRPC API to help me understand the concept of gRPC, and to make it easier for me to debug code in the larger project of the OS class I'm taking.
+
+## Concept 
+### Services Definition 
+Services are defined in [proto file](./linh.proto). gRPC uses what's called the "protobuf language" for their Interface Definition Language. This file includes: 
+    - package name : This will then be the `namespace` in all the generated code for implementation.
+    - name of the service (i.e:`DataComm`). This will be the based-class from which the service implementation class will be derived from. 
+    - methods to be implemented for the service
+    - messages definitions and their fields. 
+
+
+
 ## Compile 
 
-protos only 
-
-`make protos`
-client, server, protos:
-`make all`
+1. Generate grpc-compiled code only 
+```
+    make protos
+```
+2. Generate client, server, protos:
+```
+    make all
+```
--- linh_server.cc
+++ linh_server.cc
@@ -1,13 +1,20 @@
 #include <string>
+#include <grpc/grpc.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
 
-#include <grpcpp/grpcpp.h>
 #include "proto-src/linh.grpc.pb.h"
 
+using std::string; 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 using linh::DataComm;
+
+
 
 /* DataCommImpl is the derived class of Service
 This is where all the service methods are implemented */
@@ -15,6 +22,7 @@ This is where all the service methods are implemented */
 class DataCommImpl final : public DataComm::Service {
 
 public: 
+    DataCommImpl();
     Status SendFile(ServerContext* context, const ::linh::FileReq* request, ::linh::FileRes* response) override{
         std::string file_resp="this is the file respond"; 
         response->set_respond(file_resp);
@@ -23,7 +31,20 @@ public:
 
 };
 
+void RunServer(string server_address) {
+  
+  DataCommImpl service;
+  ServerBuilder builder;
+  
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
 int main() {
-    // Your code here
+    string server="0.0.0.0:50051"; 
+    RunServer(server);
     return 0;
 }
